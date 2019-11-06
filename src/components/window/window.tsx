b251a202e369572b764ccbe918c64f4fc053e071
Jordi Orriols <info@jordiorriols.cat>
2019-11-07 00:30:18 +0100

    Remove parent on draggable

--- src/components/window/window.tsx
+++ src/components/window/window.tsx
@@ -1,33 +1,22 @@
-
-import { Window, TitleBar, Text } from 'react-desktop/macOs';
-import Draggable from 'react-draggable';
+import { Window, TitleBar, Text } from "react-desktop/macOs";
+import Draggable from "react-draggable";
 import React from "react";
 
 interface Props {
-  title: string;
-  text: string;
-  onClose(): void;
+    title: string;
+    text: string;
+    onClose(): void;
 }
 
 const AppWindow: React.FC<Props> = props => {
-  return (
-    <Draggable
-      bounds={'parent'}
-      defaultPosition={{ x: 0, y: 0 }}
-      scale={1}
-    >
-      <Window
-        chrome
-        width="300px"
-        height="100px"
-        padding="10px"
-      >
-        <TitleBar title={props.title} controls onCloseClick={props.onClose} />
-        <Text>{props.text}</Text>
-      </Window>
-    </Draggable>
-
-  );
+    return (
+        <Draggable defaultPosition={{ x: 0, y: 0 }} scale={1}>
+            <Window chrome width="300px" height="100px" padding="10px">
+                <TitleBar title={props.title} controls onCloseClick={props.onClose} />
+                <Text>{props.text}</Text>
+            </Window>
+        </Draggable>
+    );
 };
 
 export default AppWindow;

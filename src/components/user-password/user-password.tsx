9cd1085eb79d75edc7b6d1756364064c79f94ef8
Jordi Orriols <info@jordiorriols.cat>
2019-11-07 00:34:01 +0100

    Prettier format

--- src/components/user-password/user-password.tsx
+++ src/components/user-password/user-password.tsx
@@ -4,55 +4,51 @@ import "./user-password.scss";
 import { User } from "../../data/users/users";
 
 interface Props {
-  user?: User;
-  goBack(): void;
-  success(): void;
-  failed(): void;
-  blocked: boolean;
+    user?: User;
+    goBack(): void;
+    success(): void;
+    failed(): void;
+    blocked: boolean;
 }
 
 const UserPassword: React.FC<Props> = props => {
-  const [input, setInput] = useState("");
+    const [input, setInput] = useState("");
 
-  return (
-    <div className="user-password-container">
-      {props.blocked ? (
-        <div className="user-blocked">
-          Account blocked. Try again in 5 minutes...
+    return (
+        <div className="user-password-container">
+            {props.blocked ? (
+                <div className="user-blocked">Account blocked. Try again in 5 minutes...</div>
+            ) : props.user ? (
+                <React.Fragment>
+                    <div className="user-password">
+                        <div className="user-back" onClick={props.goBack}>
+                            <ArrowBack></ArrowBack>
+                        </div>
+                        <input
+                            type="password"
+                            placeholder="Enter Password"
+                            autoComplete="off"
+                            onChange={() => {}}
+                            onKeyDown={event => {
+                                if (event.keyCode === 13) {
+                                    if (props.user && input === props.user.password) {
+                                        props.success();
+                                    } else {
+                                        setInput("");
+                                        props.failed();
+                                    }
+                                }
+                            }}
+                            value={input}
+                            onInput={(e: React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value)}
+                        ></input>
+                        <div className="user-space"></div>
+                    </div>
+                    <div className="user-remark">Touch ID or Enter Password</div>
+                </React.Fragment>
+            ) : null}
         </div>
-      ) : props.user ? (
-        <React.Fragment>
-          <div className="user-password">
-            <div className="user-back" onClick={props.goBack}>
-              <ArrowBack></ArrowBack>
-            </div>
-            <input
-              type="password"
-              placeholder="Enter Password"
-              autoComplete="off"
-              onChange={() => {}}
-              onKeyDown={event => {
-                if (event.keyCode === 13) {
-                  if (props.user && input === props.user.password) {
-                    props.success();
-                  } else {
-                    setInput("");
-                    props.failed();
-                  }
-                }
-              }}
-              value={input}
-              onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setInput(e.target.value)
-              }
-            ></input>
-            <div className="user-space"></div>
-          </div>
-          <div className="user-remark">Touch ID or Enter Password</div>
-        </React.Fragment>
-      ) : null}
-    </div>
-  );
+    );
 };
 
 export default UserPassword;
--- src/components/window-error/window-error.tsx
+++ src/components/window-error/window-error.tsx
@@ -1,20 +1,14 @@
-
-import { Window, TitleBar, Text } from 'react-desktop/macOs';
-
 import React from "react";
-import AppWindow from '../window/window';
+import AppWindow from "../window/window";
 
 interface Props {
-  title: string;
-  text: string;
-  onClose(): void;
+    title: string;
+    text: string;
+    onClose(): void;
 }
 
 const ErrorWindow: React.FC<Props> = props => {
-  return (
-    <AppWindow title={props.title} text={props.text} onClose={props.onClose}    >
-    </AppWindow>
-  );
+    return <AppWindow title={props.title} text={props.text} onClose={props.onClose}></AppWindow>;
 };
 
 export default ErrorWindow;

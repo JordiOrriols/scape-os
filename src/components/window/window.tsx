419405d3c268e417328b46090e3b53b43e40ea63
Jordi Orriols <erni-services.jordi.orriols.amate@hp.com>
2019-11-11 21:05:24 +0100

    Add bounds on windows

--- src/components/window/window.tsx
+++ src/components/window/window.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const AppWindow: React.FC<Props> = props => {
     return (
-        <Draggable defaultPosition={{ x: 0, y: 0 }} scale={1}>
+        <Draggable bounds=".window-space" defaultPosition={{ x: 0, y: 0 }} scale={1}>
             <Window chrome width="300px" height="100px" padding="10px">
                 <TitleBar title={props.title} controls onCloseClick={props.onClose} />
                 <Text>{props.text}</Text>

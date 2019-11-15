283abf66fff13c73492440bc0b06a499a58f94a3
Jordi Orriols <erni-services.jordi.orriols.amate@hp.com>
2019-11-15 20:27:53 +0100

    Add build

---
+++ build/precache-manifest.7be076e52d40f544584dfa15dcf0fb54.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8091f4db495007239bf7afa91011e1d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7222e31b8901d00d62e1",
+    "url": "/static/css/main.fdcad5bf.chunk.css"
+  },
+  {
+    "revision": "6bc3822d3373ee5b3142",
+    "url": "/static/js/2.966a2f68.chunk.js"
+  },
+  {
+    "revision": "7222e31b8901d00d62e1",
+    "url": "/static/js/main.3682776f.chunk.js"
+  },
+  {
+    "revision": "b5ecd194857583e10cb0",
+    "url": "/static/js/runtime-main.3abaa719.js"
+  },
+  {
+    "revision": "7221e1ddfc2eddbc79febb9f33b75a4c",
+    "url": "/static/media/Calendar.7221e1dd.png"
+  },
+  {
+    "revision": "2ff3caa942cf157aa02c863e311cff02",
+    "url": "/static/media/Contacts.2ff3caa9.png"
+  },
+  {
+    "revision": "23d261bd37ae6cb9e57959e02c20cae4",
+    "url": "/static/media/FaceTime.23d261bd.png"
+  },
+  {
+    "revision": "036371bc90eb8eef2b907946a346aba4",
+    "url": "/static/media/Mail.036371bc.png"
+  },
+  {
+    "revision": "b51477655c8c5af32ae4de05c1317e5b",
+    "url": "/static/media/Messages.b5147765.png"
+  },
+  {
+    "revision": "898d03a791cda0b57bd3343a6c6ed68f",
+    "url": "/static/media/Numbers.898d03a7.png"
+  },
+  {
+    "revision": "f2e2a5016c16b9abe35260958cf5a700",
+    "url": "/static/media/PadLock.f2e2a501.png"
+  },
+  {
+    "revision": "7a67212b32083628524648673f8860f5",
+    "url": "/static/media/Pages.7a67212b.png"
+  },
+  {
+    "revision": "ea878bda3f1ec67f8d40138acc4c6349",
+    "url": "/static/media/Photos.ea878bda.png"
+  },
+  {
+    "revision": "24338f6d8be7820e6d764476800ea255",
+    "url": "/static/media/Preview.24338f6d.png"
+  },
+  {
+    "revision": "8ca2476984ae6cf0f27794083ae0cff3",
+    "url": "/static/media/Reminders.8ca24769.png"
+  },
+  {
+    "revision": "f95034936a942f4da6d94f2458174930",
+    "url": "/static/media/Safari.f9503493.png"
+  },
+  {
+    "revision": "d4bbe2387f40929756852a9ac962667c",
+    "url": "/static/media/TextEdit.d4bbe238.png"
+  },
+  {
+    "revision": "9eedce810fd785353c1b714f922c3010",
+    "url": "/static/media/TimeMachine.9eedce81.png"
+  },
+  {
+    "revision": "81e341ef769e433fb3faa648b3617295",
+    "url": "/static/media/arrowback.81e341ef.svg"
+  },
+  {
+    "revision": "4c85a885a0a962bb07b6c379a39ae90a",
+    "url": "/static/media/doctor-one.4c85a885.jpg"
+  },
+  {
+    "revision": "bbbbe0e3b14decb049cc00d266aaf5a2",
+    "url": "/static/media/doctor-three.bbbbe0e3.jpg"
+  },
+  {
+    "revision": "016b99e2597c13a065d1b09a624e4687",
+    "url": "/static/media/doctor-two.016b99e2.jpg"
+  },
+  {
+    "revision": "b761330fd780a5a4b51c0327dcc09805",
+    "url": "/static/media/iMovie.b761330f.png"
+  },
+  {
+    "revision": "70e89142ea2d24e4cbb0e9ce3c8e483d",
+    "url": "/static/media/mojave-night.70e89142.png"
+  },
+  {
+    "revision": "1382c29cdb72f6c99043675d6e13b625",
+    "url": "/static/media/photon-entypo.1382c29c.ttf"
+  },
+  {
+    "revision": "2614e058b2dcb9d6e2e964730d795540",
+    "url": "/static/media/photon-entypo.2614e058.eot"
+  },
+  {
+    "revision": "bf614256dbc49f4bf2cf786706bb0712",
+    "url": "/static/media/photon-entypo.bf614256.woff"
+  },
+  {
+    "revision": "8d5da3b56749e1aaaba7ab6bf7f95880",
+    "url": "/static/media/siren.8d5da3b5.mp3"
+  }
+]);
